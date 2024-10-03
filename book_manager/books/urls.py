6a2c4c3b93de48a5c96ce3a30e43c00624dc54bf
Kateryna Ihnatieva <ignatevaekaterina3@gmail.com>
2024-10-03 16:00:02 +0300

    added urls.py and created views

---
+++ book_manager/books/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('books/', BookListCreate.as_view(), name="books-list-create"),
+    path('books/<int:pk>', BookRetrieveUpdateDestroy().as_view(), name="books-retrieve-update-destroy"),
+    path('authors/', AuthorListCreate.as_view(), name="authors-list-create"),
+    path('authors/<int:pk>', AuthorRetrieveUpdateDestroy().as_view(), name="authors-retrieve-update-destroy"),
+]
--- book_manager/books/views.py
+++ book_manager/books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.core.serializers import serialize
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class AuthorListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class AuthorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
